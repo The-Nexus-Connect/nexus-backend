afb2b36a7909a07c9fb0e08c66845bdcdae75577
hrshkshri <harshkeshari100@gmail.com>
2023-10-30 14:35:07 +0530

    update user schema

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -1,24 +1,22 @@
-const mongoose = require ("mongoose");
+const mongoose = require("mongoose");
 
-const userSchema =new   mongoose.Schema(
-    {
-    username:{type:String,required:true},
-    branch:{
-        type:String,
-        enum:[
-            "CSE(AI)",
-            "CSE(AIML)"
-        ],
+const userSchema = new mongoose.Schema(
+  {
+    username: { type: String, required: true },
+    branch: {
+      type: String,
+      enum: ["CSE(AI)", "CSE(AIML)"],
     },
     email: { type: String, required: true },
-    libId: { type: String, required:true }, 
+    libId: { type: String, required: true },
     bio: { type: String },
-    codechefId: { type: String, required:true }, 
+    codechefId: { type: String },
     password: { type: String, required: true },
-    stars:{type:Number}
-},
-{
-  timestamps: true,
-});
+    stars: { type: Number },
+  },
+  {
+    timestamps: true,
+  }
+);
 
 module.exports = mongoose.model("User", userSchema);
