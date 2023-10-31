8199244c379bb8a8f1ccf1f3301e26e657d3c42e
hrshkshri <harshkeshari100@gmail.com>
2023-10-31 09:25:32 +0530

    try catch block for login

--- src/controllers/UserControllers.js
+++ src/controllers/UserControllers.js
@@ -85,29 +85,33 @@ const registerUser = asyncHandler(async (req, res) => {
 //@route POST /api/users/login
 //@access public
 const loginUser = asyncHandler(async (req, res) => {
-  const { email, password } = req.body;
-  if (!email || !password) {
-    res.status(400);
-    throw new Error("All fields are mandatory!");
-  }
-  const user = await User.findOne({ email });
-  //compare password with hashedpassword
-  if (user && (await bcrypt.compare(password, user.password))) {
-    const accessToken = jwt.sign(
-      {
-        user: {
-          username: user.username,
-          email: user.email,
-          id: user.id,
+  try {
+    const { email, password } = req.body;
+    if (!email || !password) {
+      res.status(400);
+      throw new Error("All fields are mandatory!");
+    }
+    const user = await User.findOne({ email });
+
+    if (user && (await bcrypt.compare(password, user.password))) {
+      const accessToken = jwt.sign(
+        {
+          user: {
+            username: user.username,
+            email: user.email,
+            id: user.id,
+          },
         },
-      },
-      process.env.ACCESS_TOKEN_SECERT,
-      { expiresIn: "1d" }
-    );
-    res.status(200).json({ accessToken });
-  } else {
-    res.status(401);
-    throw new Error("email or password is not valid");
+        process.env.ACCESS_TOKEN_SECRET,
+        { expiresIn: "1d" }
+      );
+      res.status(200).json({ accessToken });
+    } else {
+      res.status(401);
+      throw new Error("email or password is not valid");
+    }
+  } catch (error) {
+    res.status(500).json({ error: error.message });
   }
 });
 
