4f3d504ae104363112ab32c1cd466576290e9f26
Harshitr10 <1012harshit@gmail.com>
2023-11-04 15:34:10 +0530

    created winner model

--- src/controllers/CodechefWinnerController.js
+++ src/controllers/CodechefWinnerController.js
@@ -1,8 +1,11 @@
 const Codechef = require("../models/contestModels/codechefModel");
 const User = require("../models/userModel");
+const Winner = require("../models/contestModels/codechefWinnerModel")
 const axios = require("axios");
 
-
+// @desc PUT start rating
+// @route PUT /api/winner/start
+// @access public
 const startRating = async (req,res)=>{
 
     try {
@@ -19,18 +22,20 @@ const startRating = async (req,res)=>{
       const response = await axios.get("https://nexus-backend-bmfi.onrender.com/api/contests/codechef/" + user.codechefId);
       const responseData = response.data;
   
-      codechef.currentRating = responseData.currentRating;
+      codechef.beforeRating = responseData.currentRating;
       await codechef.save();
       res.status(201).send({ success: true, data: codechef });
     }
   } catch (error) {
     
-    console.error('Error:', error);
-  }
+    console.log(error);  }
   
   
 }
 
+// @desc PUT end rating
+// @route PUT /api/winner/end
+// @access public
 const endRating = async (req,res)=>{
     try {
         const userData = await axios.get("https://nexus-backend-bmfi.onrender.com/api/users");
@@ -53,10 +58,36 @@ const endRating = async (req,res)=>{
         }
       } catch (error) {
         
-        console.error('Error:', error);
+        console.log(error);
       }
     
 }
 
 
-module.exports = {startRating,endRating};
+// @desc PUT calculate difference
+// @route PUT /api/winner/calculate
+// @access public
+const calcWinner = async (req,res)=>{
+  try{
+    const userData = await axios.get("https://nexus-backend-bmfi.onrender.com/api/users");
+    for (const user of userData.data) {
+      const codechef = await Codechef.findOne({ user_id: user._id });
+      const winnerCalc = await Winner.findOne({ user_id: user._id });
+
+      if (!codechef || !winnerCalc) {
+        res.status(404);
+        throw new Error("User not found");
+      }
+
+      winnerCalc.ratingDiff =  (codechef.beforeRating - codechef.afterRating);
+      winnerCalc.stars = codechef.stars;
+      await winnerCalc.save();
+      res.status(201).send({ success: true, data: winnerCalc });
+
+  }
+}
+  catch(error){
+    console.log(error);  }
+}
+
+module.exports = {startRating,endRating,calcWinner};
--- src/routes/CodechefWinnerRoutes.js
+++ src/routes/CodechefWinnerRoutes.js
@@ -2,11 +2,13 @@ const express = require("express");
 const router = express.Router();
 const {
     startRating,
-    endRating
-
-} = require("../controllers/WinnerController");
+    endRating,
+    calcWinner
+} = require("../controllers/CodechefWinnerController");
 
 router.route("/start").put(startRating);
 router.route("/end").put(endRating);
+router.route("/calculate").put(calcWinner);
+
 
 module.exports = router;
