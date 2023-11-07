8d23364ff3b44c45f8c6b72944bbc9cb1bfef65c
Harshitr10 <1012harshit@gmail.com>
2023-11-07 13:59:08 +0530

    fixed cluster

--- app.js
+++ app.js
@@ -1,5 +1,3 @@
-const cluster = require('node:cluster');
-const numCPUs = require('node:os').availableParallelism();
 const express = require("express");
 const cors = require("cors");
 const connectDb = require("./config/dbConnection");
@@ -19,14 +17,6 @@ app.use(
 );
 app.use(express.json());
 
-
-if (cluster.isPrimary) {
-  for (let i = 0; i < numCPUs; i++) {
-    cluster.fork();
-  }
-}
-
-else{
 app.use("/api/users", require("./src/routes/UserRoutes"));
 app.use("/api/contests", require("./src/routes/ContestRoutes"));
 app.use("/api/winner", require("./src/routes/CodechefWinnerRoutes"));
@@ -35,4 +25,3 @@ app.use("/api/announcements", require("./src/routes/AnnouncementsRoutes"));
 app.listen(port, () => {
   console.log(`Server is running at port ${port}`);
 });
-}
