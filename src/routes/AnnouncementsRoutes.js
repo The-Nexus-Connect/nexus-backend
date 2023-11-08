7923b97e290db1683268cb4e738caa80d9023e19
hrshkshri <harshkeshari100@gmail.com>
2023-11-09 02:54:19 +0530

    fixed announcement err form frontend

--- src/routes/AnnouncementsRoutes.js
+++ src/routes/AnnouncementsRoutes.js
@@ -2,12 +2,13 @@ const express = require("express");
 const router = express.Router();
 
 const {
-    getAnnouncements,
-    postAnnouncements,
-    getAnnouncement,
-    updateAnnouncements,
+  getAnnouncements,
+  postAnnouncements,
+  getAnnouncement,
+  updateAnnouncements,
 } = require("../controllers/AnnouncementsController");
 
 router.route("/").get(getAnnouncements).post(postAnnouncements);
+router.route("/:id").get(getAnnouncement).put(updateAnnouncements);
 
 module.exports = router;
